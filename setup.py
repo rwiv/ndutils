3b43180d9c1960989ace68d0a47853d713d6c9bd
rwiv <zc798546@gmail.com>
2025-08-31 22:35:30 +0900

    feat: update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ndutils",
-    version="0.1.1",
+    version="0.1.2",
     description="nodriver utility functions for web crawling",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rwiv/ndutils",
     packages=setuptools.find_packages(),
     install_requires=[
-        "pydantic==2.11.7",
+        "pydantic>=2.11.7",
         "zendriver>=0.13.1",
     ],
 )
