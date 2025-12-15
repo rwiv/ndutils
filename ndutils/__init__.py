7d1a8344ad0ff98a4f2c737707123face855da22
rwiv <zc798546@gmail.com>
2025-12-15 19:28:45 +0900

    feat: upgrade version

--- ndutils/__init__.py
+++ ndutils/__init__.py
@@ -3,6 +3,7 @@ from nodriver.core.config import Config  # type: ignore
 
 from .cookie import *
 from .action import *
+from .browser import *
 from .cdp import *
 from .query import *
 from .snapshot import *
